5dc1db447875591a62207ba33b2ffb7af5997a2b
karina-bernal <70300920+karina-bernal@users.noreply.github.com>
2024-12-09 13:21:11 -0600

    Update make_usecase1.sh

--- make_usecase1.sh
+++ make_usecase1.sh
@@ -4,4 +4,4 @@
 # echo $1 $2 $3
 
 # Run classification script with given arguments
-python3 /home/calo/compartido/AAA-master/automatic_processing/USECASE1_CONTINUOUS_CLASSIFICATION.py $1 $2 $3
+python3 USECASE1_CONTINUOUS_CLASSIFICATION.py $1 $2 $3
